6b5b23aba706f8e3e98cf80a5a71fb2b7af8eaa1
João Barbosa <contato.jsbarbosa@gmail.com>
2023-08-09 19:39:58 -0300

    Implemented addProducts() and removeProducts() functionality

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,20 +9,29 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Product product  = new Product();
-		
+		Product product = new Product();
+
 		System.out.println("Enter product data: ");
-		
+
 		System.out.print("Name: ");
 		product.setName(sc.nextLine());
-		
+
 		System.out.print("Price: ");
 		product.setPrice(sc.nextDouble());
-		
+
 		System.out.print("Quantity in stock: ");
 		product.setQuantity(sc.nextInt());
-		
-		System.out.println(product);
+
+		System.out.println("\nProduct data: " + product);
+		System.out.println("\n---------------------------------------------------\n");
+
+		System.out.print("Enter the number of products to be added in stock: ");
+		product.addProducts(sc.nextInt());
+		System.out.println("\nUpdate data: " + product);
+		System.out.println("\n---------------------------------------------------\n");
+		System.out.print("Enter the number of products to be removed from stock: ");
+		product.removeProducts(sc.nextInt());
+		System.out.println("\nUpdate data: " + product);
 		sc.close();
 	}
 
